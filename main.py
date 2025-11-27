14e1e53d6bc111595197c3a9987f0509c8d170dd
mraendic-lang <mraendic@tip.edu.ph>
2025-11-27 18:50:06 +0800

    added functions

---
+++ main.py
@@ -0,0 +1,31 @@
+# Add, Update, and Delete tasks
+# Mark a task as in progress or done
+# List all tasks
+# List all tasks that are done
+# List all tasks that are not done
+# List all tasks that are in progress
+print('Task Tracker')
+
+def taskAdd():
+    pass
+
+def taskUpdate():
+    pass
+
+def taskDelete():
+    pass
+
+def taskInProgress():
+    pass
+
+def taskDone():
+    pass   
+
+def taskListAll():
+    pass
+
+def taskNotDone():
+    pass
+
+def taskMark():
+    pass
